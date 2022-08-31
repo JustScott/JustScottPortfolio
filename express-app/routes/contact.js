d2af2889818a465e00718e31167c58af92b8776c
JustScott <justscottmail@protonmail.com>
2022-08-31 15:02:17 -0500

    The GMAIL API now fetches its credentials from a .env file

--- express-app/routes/contact.js
+++ express-app/routes/contact.js
@@ -4,44 +4,37 @@ const nodemailer = require('nodemailer');
 const fs = require('fs');
 const path = require('path');
 
+require('dotenv').config()
+
 const router = express.Router();
 
 function sendEmail(sendersEmail, subject, body) {
-    fs.readFile(path.join(__dirname, '../.secrets/gmail_credentials.txt'), 'utf8', (err,data) => {
-        if (err) {
-            console.log(err);
-        }else if (data) {
-            data = data.split('\n');
-            const username = data[0];
-            const password = data[1];
+    let mailTransporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env['GMAIL_ADDRESS'],
+            pass: process.env['GMAIL_PASSWORD'],
+        }
+    });
 
-            let mailTransporter = nodemailer.createTransport({
-                service: 'gmail',
-                auth: {
-                    user: username,
-                    pass: password,
-                }
-            });
-        
-            let mailDetails = {
-                from: sendersEmail,
-                to: 'telephoneemail@protonmail.com',
-                subject: subject,
-                text: ` - Sent By: -
-                        -----------
-                        ${sendersEmail}
-                        -----------
-                        \n\n${body}`,
-            };
-            
-            mailTransporter.sendMail(mailDetails, function(err, data) {
-                if(err) {
-                    console.log(err);
-                }
-            });
+    let mailDetails = {
+        from: sendersEmail,
+        to: 'telephoneemail@protonmail.com',
+        subject: subject,
+        text: ` - Sent By: -
+                -----------
+                ${sendersEmail}
+                -----------
+                \n\n${body}`,
+    };
+
+    mailTransporter.sendMail(mailDetails, function(err, data) {
+        if(err) {
+            console.log(err);
         }
     });
 }
+
 
 router.post('/contact', (req, res) => {
     data = req.body; // JavaScript object containing the parse JSON
